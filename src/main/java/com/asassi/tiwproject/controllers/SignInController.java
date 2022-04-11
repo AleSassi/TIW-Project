878aafd860aafe40c3b569f38826315b8874fbb4
AleSassi <AleSassi@users.noreply.github.com>
2022-04-11 16:02:42 +0200

    Added Sign In static page

---
+++ src/main/java/com/asassi/tiwproject/controllers/SignInController.java
@@ -0,0 +1,19 @@
+package com.asassi.tiwproject.controllers;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import javax.servlet.*;
+
+public class SignInController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
---
+++ src/main/java/com/asassi/tiwproject/controllers/SignUpController.java
@@ -0,0 +1,23 @@
+package com.asassi.tiwproject.controllers;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+public class SignUpController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
