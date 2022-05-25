4e61bf02fff3e1f07af8f8fb8b41d3230c370e86
AleSassi <AleSassi@users.noreply.github.com>
2022-05-25 17:34:59 +0200

    Added login form checker

---
+++ RIA/src/main/webapp/javascript/loginFormChecker.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', function () {
+    const form = document.getElementById("loginForm");
+
+    form.addEventListener('submit', function (e) {
+        const usernameField = document.getElementById("username");
+        const passwordField = document.getElementById("password");
+        const usernameErrorLabel = document.getElementById("usernameError");
+        const pwdErrorLabel = document.getElementById("passwordError");
+
+        let isValid = true;
+
+        //Check for null values
+        if (!usernameField.value || usernameField.value.size < 1) {
+            //Show the Error Message for Email
+            usernameErrorLabel.textContent = "You must enter your username to sign in";
+            usernameErrorLabel.hidden = false;
+            isValid = false;
+            e.preventDefault();
+        } else {
+            usernameErrorLabel.hidden = true;
+        }
+
+        if (!passwordField.value || passwordField.value.size < 8) {
+            pwdErrorLabel.textContent = "You must enter your password";
+            pwdErrorLabel.hidden = false;
+            isValid = false;
+            e.preventDefault();
+        } else {
+            pwdErrorLabel.hidden = true;
+        }
+        return isValid;
+    })
+})
